b53c0e4fc7efc0a1d9bce7c9687cd4e2a887df9a
Lewis Wade <lewis@mrwade.net>
2015-08-31 11:38:47 -0500

    Display pins to DOM

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -1,6 +1,7 @@
-app.controller("MainCtrl", ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
-	//Add Firebase Reference
-	var ref = new Firebase("https://nss-pinterested.firebaseio.com/");
-
+app.controller("MainCtrl", ["$scope", "$firebaseArray",
+  function($scope, $firebaseArray) { 
+    var pinsArray = new Firebase("https://nss-pinterested.firebaseio.com/");
+    $scope.pins = $firebaseArray(pinsArray);
+    console.log($scope.pins);
 
 }]);
